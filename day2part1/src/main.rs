b814f56df6e969b2e3400257250873d58b967a17
anguye21 <qb7j4orb@wussie.anonaddy.com>
2021-12-03 15:50:43 -0500

    feat day2part1

---
+++ day2part1/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+fn main() {
+    let filename = "assets/input-real.txt";
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let data: Vec<&str> = contents.lines().collect();
+
+    let mut horizontal: i32 = 0;
+    let mut depth: i32 = 0;
+
+    for command in data {
+        let command_vec: Vec<&str> = command.split(" ").collect();
+        let direction: &str = command_vec[0];
+        let amount: i32 = command_vec[1].parse().unwrap();
+
+        match direction {
+            "forward" => horizontal += amount,
+            "down" => depth += amount,
+            "up" => depth -= amount,
+            &_ => (),
+        }
+    }
+
+    println!("horizontal = {}", horizontal);
+    println!("depth = {}", depth);
+
+    println!("\n{}", horizontal * depth);
+}
