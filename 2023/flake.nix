f9d759ea33ce5bbeec080624e28e473b8a34f8f8
anguye21 <qb7j4orb@wussie.anonaddy.com>
2024-05-13 22:24:35 -0400

    chore(2023): convert to elixir

--- 2023/flake.nix
+++ 2023/flake.nix
@@ -1,79 +1,39 @@
 {
   inputs = {
-    opam-nix.url = "github:tweag/opam-nix";
-    flake-utils.url = "github:numtide/flake-utils";
-    # we pin opam-nix's nixpkgs to follow the flakes, avoiding using two different instances
-    opam-nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    # maintain a different opam-repository to those pinned upstream
-    opam-repository = {
-      url = "github:ocaml/opam-repository";
-      flake = false;
-    };
-    opam-nix.inputs.opam-repository.follows = "opam-repository";
-
-    # deduplicate flakes
-    opam-nix.inputs.flake-utils.follows = "flake-utils";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, flake-utils, opam-nix, ... }@inputs:
-    # create outputs for each default system
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        package = "aoc";
-        pkgs = nixpkgs.legacyPackages.${system};
-        opam-nix-lib = opam-nix.lib.${system};
-        devPackagesQuery = {
-          ocaml-lsp-server = "*";
-          ocamlformat = "*";
-          # 1.9.6 fails to build
-          ocamlfind = "*";
-          utop = "*";
-          core = "*";
-        };
-        query = {
-          ocaml-base-compiler = "*";
-        };
-        resolved-scope =
-          # recursive finds vendored dependancies in duniverse
-          opam-nix-lib.buildOpamProject' { recursive = true; } ./. (query // devPackagesQuery);
-        materialized-scope =
-          opam-nix-lib.materializedDefsToScope { sourceMap.${package} = ./.; } ./package-defs.json;
-      in rec {
-        packages = rec {
-          resolved = resolved-scope;
-          materialized = materialized-scope;
-          # to generate:
-          #   cat $(nix eval .#package-defs --raw) > package-defs.json
-          package-defs = opam-nix-lib.materializeOpamProject' { } ./. (query // devPackagesQuery);
-        };
-        defaultPackage = packages.materialized.${package};
 
-        devShells =
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
           let
-            mkDevShell = scope:
-              let
-                devPackages = builtins.attrValues
-                  (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope);
-              in pkgs.mkShell {
-                inputsFrom = [ scope.${package} ];
-                buildInputs = devPackages;
-              };
-            dev-scope =
-              # don't pick up duniverse deps
-              # it can be slow to build vendored dependancies in a deriviation before getting an error
-              opam-nix-lib.buildOpamProject' { } ./. (query // devPackagesQuery);
-          in rec {
-            resolved = mkDevShell resolved-scope;
-            materialized = mkDevShell materialized-scope;
-            # use for fast development as it doesn't building vendored sources in seperate derivations
-            # however might not build the same result as `nix build .`,
-            # like `nix develop .#devShells.x86_64-linux.resolved -c dune build` should do
-            dev = mkDevShell dev-scope;
-            default = dev;
-          };
-      }) // {
-    nixosModules.default = {
-      imports = [ ./module.nix ];
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  languages.elixir.enable = true;
+                }
+              ];
+            };
+          });
     };
-  };
 }
---
+++ 2023/src/day01.exs
@@ -0,0 +1,7 @@
+defmodule Day1 do
+  def run do
+    IO.puts("Hello World")
+  end
+end
+
+Day1.run()
